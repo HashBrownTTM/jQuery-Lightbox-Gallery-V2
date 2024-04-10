8f07d98e97abfc5d7e90f59b125a72819d19a92e
Tiego Masemola <93540733+HashBrownTTM@users.noreply.github.com>
2024-04-10 18:23:21 +0200

    Updated media links

--- main-js.js
+++ main-js.js
@@ -1,78 +1,78 @@
 $(function(){
     const arrImages = [
         {
-            src : "media/images/elena-mozhvilo-vGbkqyCRA1Q-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/elena-mozhvilo-vGbkqyCRA1Q-unsplash.webp",
             desc : "Photo by Elena Mozhvilo on Unsplash https://unsplash.com/photos/vGbkqyCRA1Q"
         },
         {
-            src : "media/images/jon-parry-C8eSYwQkwHw-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/jon-parry-C8eSYwQkwHw-unsplash.webp",
             desc : "Photo by Jon Parry on Unsplash https://unsplash.com/photos/C8eSYwQkwHw"
         },
         {
-            src : "media/images/karthik-sreenivas-rsx-joaKYrk-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/karthik-sreenivas-rsx-joaKYrk-unsplash.webp",
             desc : "Photo by Karthik Sreenivas on Unsplash https://unsplash.com/photos/rsx-joaKYrk"
         },
         {
-            src : "media/images/kaung-myat-min-n39-bU4c5kQ-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/kaung-myat-min-n39-bU4c5kQ-unsplash.webp",
             desc : "Photo by Kaung Myat Min on Unsplash https://unsplash.com/photos/n39-bU4c5kQ"
         },
         {
-            src : "media/images/will-4lbniAdMzcc-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/will-4lbniAdMzcc-unsplash.webp",
             desc : "Photo by Will on Unsplash https://unsplash.com/photos/4lbniAdMzcc"
         },
         {
-            src : "media/images/mostafa-meraji-HJgEB78b2Z8-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/mostafa-meraji-HJgEB78b2Z8-unsplash.webp",
             desc : "Photo by Mostafa Meraji on Unsplash https://unsplash.com/photos/HJgEB78b2Z8"
         }
     ]
     
     const arrVideos = [
         {
-            src : "media/videos/production_id_4114797 (720p).mp4",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/videos/production_id_4114797 (720p).mp4",
             desc : "Video by Edward Jenner from Pexels: https://www.pexels.com/video/laboratory-medical-medicine-virus-4114797/"
         },
         {
-            src : "media/videos/video (720p).mp4",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/videos/video (720p).mp4",
             desc : "Video by Pressmaster: https://www.pexels.com/video/digital-formation-of-a-dna-in-an-animated-presentation-3191572/"
         },
         {
-            src : "media/videos/video (720p) (1).mp4",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/videos/video (720p) (1).mp4",
             desc : "Video by Dan Cristian Pădureț: https://www.pexels.com/video/slow-motion-footage-of-the-movement-of-yellow-fluid-mixed-in-water-creating-abstract-3051356/"
         },
         {
-            src : "media/videos/pexels-otavio-henrique-17903408 (720p).mp4",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/videos/pexels-otavio-henrique-17903408 (720p).mp4",
             desc : "Video by Otavio Henrique: https://www.pexels.com/video/balneario-camboriu-17903408/"
         }
     ]
     
     const arrMedia = [
         {
-            src : "media/images/elena-mozhvilo-vGbkqyCRA1Q-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/elena-mozhvilo-vGbkqyCRA1Q-unsplash.webp",
             type: "image",
             desc : "Photo by Elena Mozhvilo on Unsplash https://unsplash.com/photos/vGbkqyCRA1Q"
         },
         {
-            src : "media/images/jon-parry-C8eSYwQkwHw-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/jon-parry-C8eSYwQkwHw-unsplash.webp",
             type: "image",
             desc : "Photo by Jon Parry on Unsplash https://unsplash.com/photos/C8eSYwQkwHw"
         },
         {
-            src : "media/videos/production_id_4114797 (720p).mp4",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/videos/production_id_4114797 (720p).mp4",
             type: "video",
             desc : "Video by Edward Jenner from Pexels: https://www.pexels.com/video/laboratory-medical-medicine-virus-4114797/"
         },
         {
-            src : "media/videos/pexels-otavio-henrique-17903408 (720p).mp4",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/videos/pexels-otavio-henrique-17903408 (720p).mp4",
             type: "video",
             desc : "Video by Otavio Henrique: https://www.pexels.com/video/balneario-camboriu-17903408/"
         },
         {
-            src : "media/images/will-4lbniAdMzcc-unsplash.webp",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/images/will-4lbniAdMzcc-unsplash.webp",
             type: "image",
             desc : "Photo by Will on Unsplash https://unsplash.com/photos/4lbniAdMzcc"
         },
         {
-            src : "media/videos/video (720p).mp4",
+            src : "https://ik.imagekit.io/cwattm/jQuery-Lightbox-Gallery-images/videos/video (720p).mp4",
             type: "video",
             desc : "Video by Pressmaster: https://www.pexels.com/video/digital-formation-of-a-dna-in-an-animated-presentation-3191572/"
         },
